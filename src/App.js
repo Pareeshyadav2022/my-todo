935e91f32583dba5bfd68445f61e7fe36823ae66
Pareesh Yadav <pareeshyadav@gmail.com>
2022-09-01 14:22:08 +0530

    fixed css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
 
-import { addTasks, updateTask, deleteTask } from './redux/reducer';
+import { addTasks, updateTask, deleteTask } from "./redux/reducer";
 
 function App() {
-  const ref = useRef('');
+  const ref = useRef("");
 
-  const [tampVal, setTampVal] = useState({ id: null, index: null, task: '' });
+  const [tampVal, setTampVal] = useState({ id: null, index: null, task: "" });
 
-  const [input, setInput] = useState({ id: 0, task: '' });
+  const [input, setInput] = useState({ id: 0, task: "" });
 
-  const [addProp, setAddProp] = useState('inline');
+  const [addProp, setAddProp] = useState("inline");
 
-  const [saveProp, setSaveProp] = useState('none');
+  const [saveProp, setSaveProp] = useState("none");
 
-  const reduxState = useSelector(state => state);
+  const reduxState = useSelector((state) => state);
 
   const dispatch = useDispatch();
 
   const addTodo = () => {
     input.task.trim() ? dispatch(addTasks(input)) : <h1>enter a value</h1>;
-    setInput({ task: '' });
+    setInput({ task: "" });
     ref.current.focus();
   };
 
@@ -29,7 +29,7 @@ function App() {
     console.log(reduxState.value);
   }, [reduxState]);
 
-  const changeHandler = e => {
+  const changeHandler = (e) => {
     setTampVal({ id: tampVal.id, index: tampVal.index, task: e.target.value });
     setInput({
       id: e.target.id,
@@ -40,8 +40,8 @@ function App() {
   const editTask = (value, ind) => {
     setInput({ id: value.id, task: value.task });
     setTampVal({ id: value.id, index: ind, task: value.task });
-    setAddProp('none');
-    setSaveProp('inline');
+    setAddProp("none");
+    setSaveProp("inline");
     ref.current.focus();
   };
 
@@ -51,9 +51,9 @@ function App() {
         {input.task.trim()
           ? (setTampVal({ task: input.task }),
             dispatch(updateTask(tampVal)),
-            setInput({ task: '' }),
-            setAddProp('inline'),
-            setSaveProp('none'),
+            setInput({ task: "" }),
+            setAddProp("inline"),
+            setSaveProp("none"),
             ref.current.focus())
           : null}
         ;
@@ -61,22 +61,22 @@ function App() {
     );
   };
 
-  const deleteTodo = id => {
+  const deleteTodo = (id) => {
     dispatch(deleteTask(id));
-    setAddProp('inline');
-    setSaveProp('none');
+    setAddProp("inline");
+    setSaveProp("none");
     ref.current.focus();
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Todo App</h1>
       <input
         ref={ref}
-        type='text'
+        type="text"
         value={input.task}
         id={new Date().valueOf()}
-        placeholder='Write a todo...'
+        placeholder="Write a todo..."
         onChange={changeHandler}
       />
       <button onClick={addTodo} style={{ display: addProp }}>
@@ -90,17 +90,26 @@ function App() {
           <div
             key={index}
             style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '35px',
-
-              padding: '0px',
-            }}>
+              display: "flex",
+              flex: 1,
+              border: "1px solid gray",
+              borderRadius: "8px",
+              width: "40%",
+              margin: "0 0 0.4% 30%",
+              padding: "5px",
+            }}
+          >
             <p
               key={index}
               onClick={() => editTask(x, index)}
-              style={{ margin: '10px', display: { addProp } }}>
+              style={{
+                display: { addProp },
+                textAlign: "left",
+                flex: 1,
+              }}
+            >
+              {index + 1}
+              {". "}
               {x.task}
             </p>
             <button onClick={() => deleteTodo(x.id)}>dlt</button>
